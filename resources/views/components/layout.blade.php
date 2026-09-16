017af542e2c2faabea1be414d42e140ea402123f
Maduks456 <madarsd41@gmail.com>
2026-09-16 18:51:49 +0300

    saving

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ asset('style.css') }}">
-        <title>{{$title ?? "CV"}}</title>
+        <title>{{$title ?? "Home"}}</title>
     </head>
     <body>
         <x-navigation></x-navigation>
