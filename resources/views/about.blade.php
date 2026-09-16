6998903c17fce835d434d1d5a5725cd00df84889
Maduks456 <madarsd41@gmail.com>
2026-09-16 16:58:50 +0300

    more update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,19 +10,21 @@
         <div class="main_box_column">
             <div class="about_box">
                 <div class="big_text">
-                    Contacts:
+                    Hello!
                 </div>
+                <div class="main_text">
+                    <p>
+                        Hello, My name is Madars Eduards Dāvis. Im 18 years old.<br> I havent been programming a long time, the first time I work any programming langueges was in the 9th grade about HTML and CSS.<br> Those lesons where the funnest and coolest things I had done and see a website that I made with my own hands was so amaizing. <br>I feel like my passion about programming came from those lesons and art.  
+                    </p>
+                </div>
+            </div>
+            <div class="about_box">
                 <div class="main_text">
                     <p><style></style>Gmail: <a href="mailto:ipa24.m.davis@vtdt.edu.lv">ipa24.m.davis@vtdt.edu.lv</a></p><br>
                     GitHub: <a href="https://github.com/Maduks456">https://github.com/Maduks456</a>
                 </div>
-            </div>
-            <div class="about_box">
                 <div class="big_text">
-                    Hello!
-                </div>
-                <div class="main_text">
-                    Hello, My name is Madars Eduards Dāvis. Im 18 years old. I havent been programming a long time, the first time I work any programming langueges was in the 9th grade about HTML and CSS. Those lesons where the funnest and coolest things I had done and see a website that I made with my own hands was so amaizing. 
+                    :Contacts
                 </div>
             </div>
         </div>
